9046f55f5cf1190eb58064f1a2bb23b0138759dc
Kitt Hsu <kitt.hsu@gmail.com>
2015-06-03 21:34:40 +0800

    New DDL.

---
+++ ddl.js
@@ -0,0 +1,93 @@
+'use strict'
+
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var _ = require('lodash');
+var async = require('async');
+
+function StepScript() {
+	this.strict = '\'use strict\'';
+	this.required = [];
+	this.methods = [];
+}
+
+StepScript.prototype.addRequired = function(varname, path) {
+	this.required.push(util.format('var %s = require(\'%s\');', varname, path));
+}
+
+function formatLine(t) {
+	var v = t.replace(/{/g, '{\n');
+	if (!_.endsWith(v, '}'))
+		v += ';';
+	return v.replace(/}/g, '\n}\n');
+}
+
+StepScript.prototype.addMember = function(name, func) {
+	var v = util.format('exports.%s = %s', name, func);
+	this.methods.push(formatLine(v));
+}
+
+StepScript.prototype.setExported = function(value) {
+	if (!value) {
+		this.exported = 'exports = module.exports = {}';
+	} else {
+		this.exported = 'exports = module.exports = ' + formatLine(value);
+	}
+}
+
+StepScript.prototype.text = function() {
+	var all = _.flattenDeep([this.strict, this.required, this.exported, this.methods]);
+	var v;
+	return all.reduce(function(line1, line2) {
+		if (line1) {
+			return line1 + '\n' + line2;
+		} else {
+			return line2;
+		}
+	});
+}
+
+function calcStep(box) {
+	var steps = fs.readdirSync(box);
+	var last_step = '';
+	if (steps.length > 0) {
+		last_step = steps[steps.length - 1];
+	}
+
+	var ss = new StepScript();
+	ss.setExported();
+	ss.addMember('lastStep', '\'' + last_step + '\'');
+	ss.addMember('run', 'function(schemas, cb) { \
+		// FIXME create or modify schema here and pass the cb to orm \
+		\n\t\t// You could access each schema by calling schemas.schema_talbe_name. \
+	}');
+	return ss.text();
+}
+
+(function() {
+	var box = process.argv[2];
+	if (!box)
+		throw Error('You should set a folder to save all steps at least');
+
+	if (!fs.existsSync(box))
+		fs.mkdirSync(box);
+
+	var note = process.argv[3];
+	if (note) {
+		var footprint = '' + Date.now() + '-' + note + '.js';
+	} else {
+		var footprint = '' + Date.now() + '.js';
+	}
+
+	var step = path.join(box, footprint);
+	if (fs.existsSync(step))
+		throw Error('You or some others save the same steps or you put this command in a loop');
+
+	fs.writeFile(step, calcStep(box), function(err) {
+		if (err)
+			throw err;
+	});
+
+	console.info('%s is created', footprint);
+})();
--- example/index.js
+++ example/index.js
@@ -5,70 +5,57 @@ var ormcache = require('ormcache');
 ormcache.useMemoryCache();
 ormcache.useRedisCache({
 	server: 'ip:port'
-}, function(err) {
-
-});
+}, function(err) {});
 
 ormcache.useMysql({
 	server: 'ip:port',
 	user: 'user:password'
-}, function(err) {
-
-});
+}, function(err) {});
 
 var User = ormcache.define('table_name_user', {
-	// ID, create_ts and update_ts will be default columns
+	// ID, update_ts will be default columns
 	name: {
 		type: 'string',
 		text_len: 32,
 		not_null: true
 	}
-});
+}).sync(function(err) {});
 
-User.sync(function(error) {
-});
+User.buildIndex([User.id, User.name], function(err) {});
 
 var Profile = ormcache.define('table_name_profile', {
 	name: {
 		type: 'string',
 		not_null: true
 	}
-}).set({
-	belongs_to: User
-});
+}).belongs_to(User).sync(function(err) {});
 
-Profile.sync(function(error) {
-});
-
-User.buildIndex([User.id, User.name], function(error) {
-});
+ormcache.join(User, Profile, function(err) {});
 
 var user = User.create({
 	name: 'xxx'
 });
 
-ormcache.save(key, user, function(error, saved_obj) {
-});
+ormcache.save(key, user, function(error, saved_obj) {});
 
 ormcache.update(key, user, {
 	name: 'yyy'
-}, function(error, new_obj) {
-});
+}, function(error, new_obj) {});
 
-ormcache.drop(key, user, function(error) {
-});
-
-// Update or create profile
-user.update(Profile, {
-	phone: 123456789,
-	addr: 'Road'
-}, function(error) {
-	console.log('Fail to update object|' + error);
-});
-
-ormcache.get(key, function(err, obj)) {
-
-}
+ormcache.transaction([
+	function(cb) {
+		var profile = Profile.create({
+			phone: 123456789,
+			addr: 'Road'
+		});
+		ormcache.save(null, profile, cb);
+	},
+	function(cb, saved_profile) {
+		ormcache.update(key, user, {
+			profile: saved_profile
+		}, cb);
+	}
+], function(err) {});
 
 ormcache.query()
 	.where('condition')
@@ -79,3 +66,8 @@ ormcache.query()
 	.exec(function(err, objs) {
 
 	});
+
+ormcache.get(key, function(err, obj) {});
+ormcache.cache(key, value, function(err) {});
+ormcache.flush(key, function(err) {});
+ormcache.drop(key, user, function(err) {});
