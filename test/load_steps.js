6b29a04bb3793781db7cd5fd15301d5397267af2
kitt1987 <kitt.hsu@gmail.com>
2015-06-09 16:17:23 +0800

    Connect to mysql.

--- test/load_steps.js
+++ test/load_steps.js
@@ -15,8 +15,10 @@ module.exports = {
 			size: '256M'
 		});
 		this.orm.useMysql({
-				server: 'localhost:3306',
-				account: 'root:root'
+				host: 'localhost',
+				port: 3306,
+				user: 'root',
+				password: '0000'
 			},
 			function(err) {
 				if (err) {
