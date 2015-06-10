1d2f65ede6458fece402e403117187381205c660
kitt1987 <kitt.hsu@gmail.com>
2015-06-10 16:36:08 +0800

    Define schema and index in DDL engine.

--- test/steps/1433406492804.js
+++ test/steps/1433406492804.js
@@ -1,10 +1,15 @@
 'use strict'
+var orm = require('../..');
 exports = module.exports = {}
 exports.lastStep = '1433406486564.js';
 exports.run = function(ormcache, cb) {
 	// FIXME create or modify schema here and pass the cb to orm 
 	// You could access each schema by calling ormcache.schemas[schema_talbe_name].
 	var User = ormcache.schemas['User'];
-	User.buildIndex('iId_Name', [User.id, User.name]);
+	User.buildIndex({
+		name: 'iId_Name',
+		column: [User.id, User.name],
+		type: orm.BtreeIndex
+	});
 	cb(User);
 }
