643126295c694c5fa54b4cd2bf4a94c86eeffdc7
kitt1987 <kitt.hsu@gmail.com>
2015-06-11 11:53:07 +0800

    Define schema.

--- test/steps/1433406486564.js
+++ test/steps/1433406486564.js
@@ -5,12 +5,16 @@ exports.lastStep = '';
 exports.run = function(ormcache, cb) {
 	// FIXME create or modify schema here and pass the cb to orm 		
 	// You could access each schema by calling schemas.schema_talbe_name.
-	var User = ormcache.define('User', {
+	var User = ormcache.define('User', [{
 		name: 'name',
 		type: orm.String,
 		len: 32,
 		not_null: true
-	});
+	}, {
+		name: 'age',
+		type: orm.SmallInteger,
+		not_null: true
+	}]);
 
 	cb(User);
 }
