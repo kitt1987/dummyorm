f09e960b1a131e5389bfc792086146703396b847
Kitt Hsu <kitt.hsu@gmail.com>
2015-06-24 18:05:38 +0800

    Simplify condition.

--- test/conditions.js
+++ test/conditions.js
@@ -1,36 +1,31 @@
 'use strict'
 
-var Condition = require('../lib/dml/condition');
+var $ = require('../lib/dml/condition').$;
+var Column = require('../lib/schema').Column;
+var Datatype = require('../lib/ddl/datatype');
 
 process.on('uncaughtException', function(err) {
 	console.error(err.stack);
 });
 
 module.exports = {
-	simpleNumber: function(test) {
-		var c = new Condition();
-		test.throws(function() {
-			c.gt('aaa', 1);
-		});
-		test.throws(function() {
-			c.gt(1, 'bbb');
-		});
+	simple: function(test) {
+		test.equal('a>b ', $('a>b'));
+		test.equal('a > b ', $('a', '>', 'b'));
 		test.done();
 	},
-	simpleString: function(test) {
-		var c = new Condition();
-		test.equal('key=\'value\'', c.eq('key', 'value').sql);
-		test.done();
-	},
-	simpleAnd: function(test) {
-		var c1 = new Condition();
-		c1.eq('key1', 'value1');
-		var c2 = new Condition();
-		c2.eq('key2', 'value2');
-		var c3 = new Condition();
-		c3.gt(1, 3);
-		c1.and(c2, c3);
-		test.equal('key1=\'value1\' AND (key2=\'value2\' AND 1>3)', c1.sql);
+	column: function(test) {
+		var c = new Column({
+			name: 'Name',
+			type: Datatype.String
+		});
+		test.equal('Name = \'a\' ', $(c, '=', 'a'));
+		test.equal('Name =\'a\' ', $(c, '=\'a\''));
+		var c2 = new Column({
+			name: 'age',
+			type: Datatype.Integer
+		});
+		test.equal('Name = \'a\' AND( age >10 ) ', $(c, '=', 'a', 'AND(', c2, '>10', ')'));
 		test.done();
 	}
 }
