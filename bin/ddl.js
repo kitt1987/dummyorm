8b5ff0f16031b5c8166755fd717b503733cd7c38
Kitt Hsu <kitt.hsu@gmail.com>
2015-06-28 09:06:33 +0800

    Merge from different paths.

--- bin/ddl.js
+++ bin/ddl.js
@@ -36,7 +36,7 @@ StepScript.prototype.setExported = function(value) {
 	if (!value) {
 		this.exported = 'exports = module.exports = {};';
 	} else {
-		this.exported = 'exports = module.exports = ' + formatLine(value) + ';';
+		this.exported = 'exports = module.exports = ' + formatLine(value);
 	}
 };
 
@@ -74,16 +74,171 @@ function calcStep(database) {
 	return ss.text();
 }
 
+function defineOptions() {
+	var ArgumentParser = require('argparse').ArgumentParser;
+	var parser = new ArgumentParser({
+		version: '0.0.0',
+		addHelp: true,
+		description: 'Unit testing framework'
+	});
+
+	parser.addArgument(
+		['-d', '--dir'], {
+			action: 'append',
+			help: 'directory steps placed. eg. -d dir1 -d dir2'
+		}
+	);
+
+	parser.addArgument(
+		['-m', '--merge'], {
+			action: 'storeTrue',
+			help: 'merge all steps into the 1st directory. eg. -m -d dir1 -d dir2'
+		}
+	);
+
+	parser.addArgument(
+		['-val', '--validate'], {
+			action: 'storeTrue',
+			help: 'validate all steps. eg. -v -d dir1 -d dir2'
+		}
+	);
+
+	parser.addArgument(
+		['-c', '--compact'], {
+			action: 'storeTrue',
+			help: 'compact all steps into 1 step placed in the 1st directory. eg. -c -d dir1 -d dir2'
+		}
+	);
+
+	parser.addArgument(
+		['-n', '--name'], {
+			help: 'partial name of step to be created. eg. -n name'
+		}
+	);
+
+	return parser.parseArgs();
+}
+
+// ocddl -m -d dir1 -d dir2 to merge steps into the 1st dir
+// ocddl -d dir1 to create a new step
+// ocddl -v -d dir1 -d dir2 to validate all steps in dir1 and dir2
+// ocddl -c -d dir1 -d dir2 to compact steps in dir1 and dir2 into 1 step saved in dir1
+
+function workInTmp(done) {
+	var tmp = '.' + require('crypto').randomBytes(16).toString('hex');
+	fs.mkdir(tmp, function(err) {
+		if (err) {
+			console.log(colors.red('Fail to make temp directory due to ') + err);
+			throw err;
+		}
+
+		done(tmp);
+	});
+}
+
+function updateLastSteps(tmp, dir) {
+	var steps = _.map(fs.readdirSync(tmp), function(step) {
+		return function(lastStep, cb) {
+			var stepPath = path.join(tmp, step);
+			var modulePath = path.relative(path.dirname(module.filename), stepPath);
+			var stepModule = require(modulePath);
+			if (lastStep !== stepModule.lastStep) {
+				fs.writeFile(stepPath, util.format('\nexports.lastStep = \'%s\';', lastStep),
+					{
+						flag: 'a'
+					},
+					function(err) {
+						if (err) {
+							throw err;
+						}
+
+						fs.rename(stepPath, path.join(dir, path.basename(stepPath)), function(err) {
+							if (err) {
+								throw err;
+							}
+
+							cb(null, step);
+						});
+					});
+				return;
+			}
+
+			fs.rename(stepPath, path.join(dir, path.basename(stepPath)), function(err) {
+				if (err) {
+					throw err;
+				}
+
+				cb(null, step);
+			});
+		};
+	});
+	steps[0] = steps[0].bind(null, '');
+	async.waterfall(steps, function(err, result) {
+		if (err) {
+			console.log(colors.red('Fail to merge steps due to ' + err));
+			throw err;
+		}
+
+		fs.rmdirSync(tmp);
+	});
+}
+
+function mergeSteps(dir, steps, tmp) {
+	async.eachSeries(steps, function(step, cb) {
+		fs.rename(step, path.join(tmp, path.basename(step)), cb);
+	}, function(err) {
+		if (err) {
+			console.log(colors.red('Fail to merge file due to ' + err));
+			throw err;
+		}
+
+		updateLastSteps(tmp, dir);
+	});
+}
+
 (function() {
 	try {
-		var database = process.argv[2];
+		console.log(colors.yellow('Make sure run this under you project directory!!'));
+		var args = defineOptions();
+		if (!args.dir || args.dir.length === 0) {
+			console.error(colors.red('You should indicate 1 directory at least!'));
+			return;
+		}
+
+		if (_.uniq(args.dir).length !== args.dir.length) {
+			console.error(colors.red('You\'ve just input same directories. Check the -d'));
+			return;
+		}
+
+		if (args.merge) {
+			var steps = _.map(args.dir, function(dir) {
+				return _.map(fs.readdirSync(dir), function(step) {
+					return path.join(dir, step);
+				});
+			});
+
+			steps = _.flattenDeep(steps).sort();
+			if (steps.length === 0) {
+				console.log(colors.yellow('Nothing loaded'));
+				return;
+			}
+
+			workInTmp(mergeSteps.bind(null, args.dir[0], steps));
+			return;
+		}
+
+		if (args.validate) {}
+
+		if (args.compact) {}
+
+		var database = args.dir[0];
 		if (!database)
 			throw Error('You should set a folder to save all steps at least');
 
 		if (!fs.existsSync(database))
 			fs.mkdirSync(database);
 
-		var note = process.argv[3];
+		var note = args.name;
 		var footprint;
 		if (note) {
 			footprint = '' + Date.now() + '-' + note + '.js';
@@ -102,6 +257,6 @@ function calcStep(database) {
 
 		console.info(colors.green(footprint + ' is created'));
 	} catch (err) {
-		console.info(colors.red(err));
+		console.error(colors.red(err.stack));
 	}
 })();
