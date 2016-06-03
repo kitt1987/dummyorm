14326f685fecdde88bcfb187be130d968f46ba7e
Kitt Hsu <kitt.hsu@gmail.com>
2016-06-03 14:18:44 +0800

    fix private method binder and promise chain

--- test/test_db/1433406492804.js
+++ test/test_db/1433406492804.js
@@ -3,7 +3,6 @@ exports = module.exports = {};
 exports.lastStep = '1433406486564.js';
 exports.run = function(orm) {
   var User = orm.schema.User;
-  console.log(User);
   User.buildIndex({
     name: 'iId_Name',
     column: [User.id, User.uid],
