9335adefee014257564d97a51566d978eb60f612
Kitt Hsu <kitt.hsu@gmail.com>
2016-06-04 22:46:23 +0800

    keep order of schema loading as they defined.

--- benchmark/bmdb/1435502537524.js
+++ benchmark/bmdb/1435502537524.js
@@ -4,21 +4,19 @@ exports = module.exports = {};
 exports.lastStep = '';
 exports.run = function(ormcache, done) {
 
-	// FIXME create or modify schema here and call done() to save changes.
-	// You could access each schema by calling ormcache[schemaTalbeName].
-	ormcache.define('benchData', [{
-		name: 'alpha',
-		type: orm.Integer
-	}, {
-		name: 'beta',
-		type: orm.String,
-		len: 128
-	}, {
-		name: 'pi',
-		type: orm.Float
-	}], {
-		engine: 'Memory'
-	})
+  ormcache.define('benchData', [{
+    name: 'alpha',
+    type: orm.Integer
+  }, {
+    name: 'beta',
+    type: orm.String,
+    len: 128
+  }, {
+    name: 'pi',
+    type: orm.Float
+  }], {
+    engine: 'Memory'
+  });
 
-	done();
+  done();
 };
